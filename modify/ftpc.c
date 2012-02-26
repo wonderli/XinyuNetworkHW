23e1ace16ed001785f52524012b80f73668958b4
Xinyu Li <lixinyu2268@gmail.com>
2012-02-25 21:33:57 -0500

    fix ftpc.c

--- modify/ftpc.c
+++ modify/ftpc.c
@@ -126,7 +126,7 @@ int file_send (int sck, char *filename, struct sockaddr_in sin_addr)
                 //send (sck, read_file_buf, nread+24+sockaddr_in_size, 0);
                 bcopy(read_file_buf,tcpd_msg.tcpd_contents, nread+24);
                 //SEND (sck, read_file_buf, nread+24+sockaddr_in_size, 0);
-                SEND (sck, (char *)&tcpd_msg, nread+24+16, 0);
+                SEND (sck, (char *)&tcpd_msg, nread+24+sizeof(struct sockaddr_in), 0);
                 usleep(10000);
         }
         else if (nread == (MAXBUF-24))
@@ -134,7 +134,8 @@ int file_send (int sck, char *filename, struct sockaddr_in sin_addr)
                 //send (sck, read_file_buf, nread+24+sockaddr_in_size, 0);
                 //SEND (sck, read_file_buf, nread+24+sockaddr_in_size, 0);
                 bcopy(read_file_buf,tcpd_msg.tcpd_contents,nread+24);
-                SEND (sck, (char *)&tcpd_msg, MAXBUF+16, 0);
+                //SEND (sck, (char *)&tcpd_msg, MAXBUF+16, 0);
+                SEND (sck, (char *)&tcpd_msg, MAXBUF+sizeof(struct sockaddr_in), 0);
                 bzero(read_file_buf, MAXBUF);
 
                 usleep(10000);
@@ -145,7 +146,7 @@ int file_send (int sck, char *filename, struct sockaddr_in sin_addr)
                                 bcopy(read_file_buf,tcpd_msg.tcpd_contents,nread);
                                 //send (sck, read_file_buf, nread, 0);
                                 //SEND (sck, read_file_buf, nread, 0);
-                                SEND (sck, (char *)&tcpd_msg, nread+16, 0);
+                                SEND (sck, (char *)&tcpd_msg, nread+sizeof(struct sockaddr_in), 0);
                                 /*printf("The nread is %d", nread);*/
                                 bzero(read_file_buf, MAXBUF);
                                 usleep(10000);
@@ -156,7 +157,7 @@ int file_send (int sck, char *filename, struct sockaddr_in sin_addr)
                                 bcopy(read_file_buf,tcpd_msg.tcpd_contents,MAXBUF);
                                 //send (sck, read_file_buf, MAXBUF, 0);
                                 //SEND (sck, read_file_buf, MAXBUF, 0);
-                                SEND (sck, (char *)&tcpd_msg, MAXBUF+16, 0);
+                                SEND (sck, (char *)&tcpd_msg, MAXBUF+sizeof(struct sockaddr_in), 0);
                                 bzero(read_file_buf, MAXBUF);
                                 usleep(10000);
                         }
