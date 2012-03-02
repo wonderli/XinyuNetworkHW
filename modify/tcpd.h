1f6d770ea471ba8c3ef3d51ec7e8abb911c6e53f
Xinyu Li <lixinyu2268@gmail.com>
2012-03-01 23:00:18 -0500

    add packet formate

--- modify/tcpd.h
+++ modify/tcpd.h
@@ -15,10 +15,30 @@
 #define TROLL_PORT_M1 3870
 #define TROLL_PORT_M2 3880
 #define MAXBUF 1000
+typedef struct packet_data {
+        int seq_num;
+        int ack;
+        int ack_seq;
+        int length;
+        int fin;
+        int fin_ack;
+        int stop;
+        char data[MAXBUF];
+} PACKET_DATA;
+typedef struct packet {
+        struct sockaddr_in msg_header;
+        PACKET_DATA msg;
+        unsigned long checksum;
+} PACKET;
 typedef struct TCPD_MSG {
         struct sockaddr_in tcpd_header;
-        char tcpd_contents[MAXBUF];
+        char tcpd_contents[sizeof(struct packet)];
+
 } TCPD_MSG;
+typedef struct troll_msg {
+	struct sockaddr_in troll_header;
+	char msg_contents[sizeof(struct TCPD_MSG)];
+} TROLL_MSG;
 
 int SEND(int socket, const void *buffer, size_t len, int flags);
 int RECEIVE(int socket, void *buffer, size_t length, int flags);
