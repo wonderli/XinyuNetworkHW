246162c6782b51c1ec6a344c7ab83fac5883626d
Xinyu Li <lixinyu2268@gmail.com>
2012-03-04 14:29:59 -0500

    test

--- modify/timer.c
+++ modify/timer.c
@@ -109,13 +109,12 @@ int main()
                                         ptr = ptr->next;
                                         time_msg_send.seq = expire_node->seq;
                                         time_msg_send.action = EXPIRE;
-
+                                        time_msg_send.time = -1;
                                         if(sendto(sock_timer_send, &time_msg_send, sizeof(time_msg_send), 0, (struct sockaddr*)&timer_send_addr, sizeof(struct sockaddr_in)) < 0);
                                         {
                                                 perror("\nTIMER SEND ERROR\n");
                                                 exit(1);
                                         }
-
 
                                         remove_node(expire_node);
                                         time_list->len--;
