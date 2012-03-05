0f5bff255718f05a70612297cdffa1e0a9d2a116
Xinyu Li <lixinyu2268@gmail.com>
2012-03-05 11:35:19 -0500

    test

--- modify/tcpd.c
+++ modify/tcpd.c
@@ -13,8 +13,8 @@ int RECV(int socket, void *buffer, size_t len, int flags) {
 	struct sockaddr_in srv_addr;
 //	struct hostent *hp;
 	srv_addr.sin_family = AF_INET;
-	//srv_addr.sin_port = htons(TCPD_PORT);
-	srv_addr.sin_port = htons(TCPD_PORT_FTPS);
+	srv_addr.sin_port = htons(TCPD_PORT);
+	//srv_addr.sin_port = htons(TCPD_PORT_FTPS);
 	srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	int srv_addr_length = sizeof(struct sockaddr_in);
         int ret;
