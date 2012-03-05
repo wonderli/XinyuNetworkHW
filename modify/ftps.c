d0aa423c95268650951050b2e9a69c13bc8ad5c7
Xinyu Li <lixinyu2268@gmail.com>
2012-03-04 21:46:41 -0500

    test

--- modify/ftps.c
+++ modify/ftps.c
@@ -60,6 +60,43 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+        /* put all zeros in buffer (clear) */
+	bzero(buf,MAXBUF);
+	/* read from msgsock and place in buf */
+        printf("\nBEGIN TO RECV\n");
+	int nread = 0; /* the number read from socket*/
+	if((nread = RECV(sock, &recv_msg, sizeof(TCPD_MSG),0)) < 0) {
+		perror("error reading on stream socket");
+		exit(1);
+	} 
+	printf("Server receives: %s\n", recv_msg.packet.data);
+        char *filename;
+        char *filepath;
+        uint32_t file_size = 0;
+        uint32_t file_size_local = 0;
+        filename = (char*)malloc(20);
+        filepath = (char*)malloc(MAXBUF);
+        //bcopy(buf, &file_size, sizeof(int));
+        bcopy(recv_msg.packet.data, &file_size, sizeof(int));
+        file_size_local = ntohl(file_size);
+        //bcopy(buf+4, filename, 20);
+        bcopy(recv_msg.packet.data+4, filename, 20);
+        strcpy(filepath, "./recv/");
+        strcat(filepath, filename);
+        printf("The file length is %d\n", file_size_local);
+        printf("The file name is %s\n", filepath);
+	/*Create the receive file folder and file*/
+	int fd = 0;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	if(access("recv",F_OK) == -1)
+	{
+		mkdir("recv",S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	}
+	if ((fd = open(filepath, O_WRONLY|O_CREAT, mode))< 0)
+	{
+		perror("File open error");
+		return 1;
+	}
 
 	//if(write(fd,buf+24, nread-24) < 0)
 //	if((nread = RECV(sock, &recv_msg, sizeof(TCPD_MSG),0)) < 0) {
@@ -78,63 +115,20 @@ int main(int argc, char *argv[])
 //        test_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 //        int test_addr_length = sizeof(struct sockaddr);
 
-        int nread = 0; /* the number read from socket*/
-        char *filename;
-        char *filepath;
-        uint32_t file_size = 0;
-        uint32_t file_size_local = 0;
-        int fd = 0;
-        mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	for(;;)
+	{
+		bzero(recv_msg.packet.data, MAXBUF);
 
-        for(;;)
-        {
-                bzero(recv_msg.packet.data, MAXBUF);
+		RECV(sock, &recv_msg, sizeof(TCPD_MSG), 0);
+		if(recv_msg.packet.fin == 1)
+		{
+			printf("\nFile: %s, length: %d has been transmitted!\n", filename, file_size_local);
+			close(fd);
+			close(sock);
+			exit(0);
 
-                RECV(sock, &recv_msg, sizeof(TCPD_MSG), 0);
-                if(recv_msg.packet.seq_num == 0)
-                {
-
-
-                        /* put all zeros in buffer (clear) */
-                        bzero(buf,MAXBUF);
-                        /* read from msgsock and place in buf */
-                        if((nread = RECV(sock, &recv_msg, sizeof(TCPD_MSG),0)) < 0) {
-                                perror("error reading on stream socket");
-                                exit(1);
-                        } 
-                        printf("Server receives: %s\n", recv_msg.packet.data);
-                        filename = (char*)malloc(20);
-                        filepath = (char*)malloc(MAXBUF);
-                        //bcopy(buf, &file_size, sizeof(int));
-                        bcopy(recv_msg.packet.data, &file_size, sizeof(int));
-                        file_size_local = ntohl(file_size);
-                        //bcopy(buf+4, filename, 20);
-                        bcopy(recv_msg.packet.data+4, filename, 20);
-                        strcpy(filepath, "./recv/");
-                        strcat(filepath, filename);
-                        printf("The file length is %d\n", file_size_local);
-                        printf("The file name is %s\n", filepath);
-                        /*Create the receive file folder and file*/
-                        if(access("recv",F_OK) == -1)
-                        {
-                                mkdir("recv",S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-                        }
-                        if ((fd = open(filepath, O_WRONLY|O_CREAT, mode))< 0)
-                        {
-                                perror("File open error");
-                                return 1;
-                        }
-
-                }
-                if(recv_msg.packet.fin == 1)
-                {
-                        printf("\nFile: %s, length: %d has been transmitted!\n", filename, file_size_local);
-                        close(fd);
-                        close(sock);
-                        exit(0);
-
-                }
-                write(fd, recv_msg.packet.data, sizeof(recv_msg.packet.data));
+		}
+			write(fd, recv_msg.packet.data, sizeof(recv_msg.packet.data));
 
 	}
 	
